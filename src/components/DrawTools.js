1c8b7bdb2682479b2a7a4dc73ce6530e29c8f543
Jussi Surma-Aho <L4929@student.jamk.fi>
2019-06-11 13:54:32 +0300

    Button actually looks legit!

--- src/components/DrawTools.js
+++ src/components/DrawTools.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { EditControl } from 'react-leaflet-draw'
 import L from 'leaflet'
+import 'leaflet-draw'
 import { FeatureGroup } from 'react-leaflet'
 
 // empty icon for the text field
@@ -19,9 +20,9 @@ L.Draw.MarkerTextBox = L.Draw.Marker.extend({
 	initialize: function (map, options) {
 		// important to have a unique type, so that it won't get mixed up with other elements
 		this.type = 'textbox';
-		this.featureTypeCode = 'textbox';
+    this.featureTypeCode = 'textbox';
 		L.Draw.Feature.prototype.initialize.call(this, map, options);
-	}
+  }
 });
 
 //Redefinitions d'éléments de DrawToolbar 
@@ -58,7 +59,7 @@ L.DrawToolbar.include ({
 			handler: new L.Draw.Marker(map, this.options.marker),
 			title: L.drawLocal.draw.toolbar.buttons.marker
 		},{
-			enabled: this.options.marker,
+      enabled: this.options.marker,
 			handler: new L.Draw.MarkerTextBox(map, this.options.marker),
 			title: 'Write text'
 		}];
@@ -72,11 +73,10 @@ class DrawTools extends Component {
 			e.layer.remove();
 			return;
     }
-    if (e.layerType === 'marker') {
-      e.layer.bindTooltip('say hello to mr. akers');
+    if (e.layerType === 'textbox') {
+      e.layer.bindTooltip('say hello to mr. akers', {permanent: true});
     }
-		let type = e.layerType; // from the example; isn't used right now, but may be relevant in the future
-		let layer = e.layer;		
+		let layer = e.layer;
 		let geoJSON = layer.toGeoJSON();
 		console.log(JSON.stringify(geoJSON, null, 4)); // makes the output readable in the console
 	}
