2e680ebc349e24a24be570b9c8321b80df1b8d72
Joni Laukka <joni.laukka.overflow@gmail.com>
2019-06-26 10:09:00 +0300

    New task form shows only if the user has login token.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,16 +4,20 @@ import TaskItem from './TaskItem';
 
 class TaskList extends React.Component{
   render(){
+    let token = sessionStorage.getItem('token');
+
     return ReactDOM.createPortal(
       <div className='tasklist'>
-      <div className='task-form'>
-        <input type='text' placeholder='Task name'></input>
-        <textarea placeholder='Task description'></textarea>
-        <button>Add new task</button>
-      </div>
-      <TaskItem text='asd' description='asudhaiud'/>
-      <TaskItem text='asd2' description='q7213761923769821'/>
-      <TaskItem text='asd3' description='tehtävä3'/>
+        {token && 
+          <div className='task-form'>
+          <input type='text' placeholder='Task name'></input>
+          <textarea placeholder='Task description'></textarea>
+          <button>Add new task</button>
+          </div>
+        }
+        <TaskItem text='asd' description='asudhaiud'/>
+        <TaskItem text='asd2' description='q7213761923769821'/>
+        <TaskItem text='asd3' description='tehtävä3'/>
       </div>,
       document.getElementById('tasklist')
     );
