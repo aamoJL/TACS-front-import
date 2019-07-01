44ad20b0deb2a7da794cd93fa20c9b912f384022
Taneli Riihimäki <m3034@student.jamk.fi>
2019-07-01 12:59:47 +0300

    Add text functionalities etc.

--- src/components/DrawTools.js
+++ src/components/DrawTools.js
@@ -1,6 +1,65 @@
 import React, { Component } from "react";
 import { EditControl } from "react-leaflet-draw";
+import L from "leaflet";
+import "leaflet-draw";
 import { FeatureGroup, Marker, Polygon, Polyline } from "react-leaflet";
+
+// class for text field
+L.Draw.MarkerTextBox = L.Draw.Marker.extend({
+  options: {
+    icon: L.divIcon({
+      className: "", // empty class to override default styling
+      iconSize: [20, 20],
+      iconAnchor: [10, 20]
+    }),
+    repeatMode: false,
+    interactive: true
+  },
+  initialize: function(map, options) {
+    this.type = "textbox"; // important to have a unique type, so that it won't get mixed up with other elements
+    this.featureTypeCode = "textbox";
+    L.Draw.Feature.prototype.initialize.call(this, map, options);
+  }
+});
+
+// Overriding default toolbar
+// Just adding one new button though lol
+L.DrawToolbar.include({
+  getModeHandlers: function(map) {
+    return [
+      {
+        enabled: this.options.polyline,
+        handler: new L.Draw.Polyline(map, this.options.polyline),
+        title: L.drawLocal.draw.toolbar.buttons.polyline
+      },
+      {
+        enabled: this.options.polygon,
+        handler: new L.Draw.Polygon(map, this.options.polygon),
+        title: L.drawLocal.draw.toolbar.buttons.polygon
+      },
+      {
+        enabled: this.options.rectangle,
+        handler: new L.Draw.Rectangle(map, this.options.rectangle),
+        title: L.drawLocal.draw.toolbar.buttons.rectangle
+      },
+      {
+        enabled: this.options.circle,
+        handler: new L.Draw.Circle(map, this.options.circle),
+        title: L.drawLocal.draw.toolbar.buttons.circle
+      },
+      {
+        enabled: this.options.marker,
+        handler: new L.Draw.Marker(map, this.options.marker),
+        title: L.drawLocal.draw.toolbar.buttons.marker
+      },
+      {
+        enabled: this.options.marker,
+        handler: new L.Draw.MarkerTextBox(map, this.options.marker),
+        title: "Write text"
+      }
+    ];
+  }
+});
 
 class DrawTools extends Component {
   constructor(props) {
@@ -12,10 +71,54 @@ class DrawTools extends Component {
   }
 
   _onCreated = e => {
-    let type = e.layerType; // from the example; isn't used right now, but may be relevant in the future
-    let layer = e.layer;
+    // check if a drawn polyline has just one point in it
+    if (e.layerType === "polyline" && e.layer.getLatLngs().length === 1) {
+      e.layer.remove();
+      return;
+    }
+
+    if (e.layerType === "textbox") {
+      // have to create tooltip as a DOM element to allow text selecting. maybe
+      let tooltip = L.DomUtil.create("div", "editable");
+      tooltip.innerHTML =
+        '<div contenteditable="true" placeholder="Click here and type"></div>';
+
+      e.layer.bindTooltip(tooltip, {
+        permanent: true,
+        direction: "bottom",
+        interactive: true
+      });
+
+      // disable dragging when cursor is over marker (tooltip)
+      // clicking on tooltip fires the marker's click handler, hence e.layer.on
+      e.layer.on("mouseover", function() {
+        e.layer._map.dragging.disable();
+      });
+
+      // enable dragging again when cursor is out of marker (tooltip)
+      e.layer.on("mouseout", function() {
+        e.layer._map.dragging.enable();
+      });
+
+      // show placeholder text again upon emptying textbox
+      e.layer.on("keyup", function() {
+        // when the text area is emptied, a <br> appears
+        // manually removing it so that the placeholder text can show
+        if (
+          tooltip.innerHTML ===
+            '<div placeholder="Click here and type" contenteditable="true"><br></div>' ||
+          tooltip.innerHTML ===
+            '<div placeholder="Click here and type" contenteditable="true"><div><br></div></div>'
+        ) {
+          tooltip.innerHTML =
+            '<div placeholder="Click here and type" contenteditable="true"></div>';
+        }
+      });
+    } // end if (e.layerType === "textbox")
+
+    // turning layer data to GeoJSON
     this.makeGeoJSON(e.layer);
-  };
+  }; // end _onCreated
 
   _onEditMove = e => {
     console.log("_onEditMove e:");
@@ -36,13 +139,13 @@ class DrawTools extends Component {
     // this.props.sendGeoJSON(e.poly);
   };
 
-  _onEditDeleteStart = () => {
+  _onEditDeleteStart() {
     this.setState({ editModeActive: true });
-  };
+  }
 
-  _onEditDeleteStop = () => {
+  _onEditDeleteStop() {
     this.setState({ editModeActive: false });
-  };
+  }
 
   _onDeleted = e => {
     console.log(e.layers._layers);
@@ -114,7 +217,7 @@ class DrawTools extends Component {
               repeatMode: true, // allows using the tool again after finishing the previous shape
               shapeOptions: {
                 color: "#f9f10c",
-                opacity: 1
+                opacity: 1 // affects the outline only. for some reason it wasn't at full opacity, so this is needed for more clarity
               }
             },
             rectangle: {
@@ -140,12 +243,9 @@ class DrawTools extends Component {
               }
             },
             marker: {
-              repeatMode: true
+              repeatMode: false
             },
             circlemarker: false
-          }}
-          edit={{
-            marker: true
           }}
         />
 
