47daca8b39771e1397c3fc7d3e42dfd5026517ce
Taneli Riihimäki <m3034@student.jamk.fi>
2019-07-11 12:56:35 +0300

    Get scores and faction colors from the server

--- src/components/ScoreCounter.js
+++ src/components/ScoreCounter.js
@@ -4,8 +4,8 @@ export default class ScoreCounter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      factionsScores: [],
-      testGameid: ""
+      scores: null,
+      testGameid: "a1231e2b-aa29-494d-b687-ea2d48cc23df"
     };
   }
 
@@ -22,20 +22,47 @@ export default class ScoreCounter extends Component {
       }
     )
       .then(res => res.json())
-      .then(scores => console.log(scores))
+      .then(scores => {
+        console.log(scores);
+        this.setState({
+          scores
+        });
+      })
       .catch(error => {
         console.log(error);
       });
   };
 
+  componentWillMount() {
+    this.getScoresAndFactions();
+  }
+
   render() {
     return (
       <div className="scoreContainer">
-        <span className="scoreCircle" />
-        <div className="scoreBlock">482</div>
+        <span
+          className="scoreCircle"
+          style={{
+            backgroundColor: this.state.scores
+              ? this.state.scores[0].faction.colour
+              : "red"
+          }}
+        />
+        <div className="scoreBlock">
+          {this.state.scores ? this.state.scores[0].score : 0}
+        </div>
         <div className="scoreDivider" />
-        <div className="scoreBlock">1278</div>
-        <span className="scoreCircle" />
+        <div className="scoreBlock">
+          {this.state.scores ? this.state.scores[1].score : 0}
+        </div>
+        <span
+          className="scoreCircle"
+          style={{
+            backgroundColor: this.state.scores
+              ? this.state.scores[1].faction.colour
+              : "blue"
+          }}
+        />
       </div>
     );
   }
