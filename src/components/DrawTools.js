0336bfaaa4818d3ec3453eb3e357e3f8138af644
Jussi Surma-Aho <L4929@student.jamk.fi>
2019-07-22 09:59:10 +0300

    removed unnecessary code and comments. still borked

--- src/components/DrawTools.js
+++ src/components/DrawTools.js
@@ -79,11 +79,7 @@ class DrawTools extends Component {
       allLayers: [],
       editModeActive: false,
       deleteModeActive: false,
-      geoJSONLayer: null,
-      currentText: null,
-      createdTextBox: null,
-      layerAmount: null,
-      createdMarker: null
+      geoJSONLayer: null
     };
   }
 
@@ -96,82 +92,11 @@ class DrawTools extends Component {
     }
 
     if (e.layerType === "textbox") {
-      /*
-      // have to create tooltip as a DOM element to allow text selecting. maybe
-      let tooltip = L.DomUtil.create("div", "editable");
-
-      // need ids for tooltips to be able to add a blur event to them
-      tooltip.innerHTML =
-        '<div contenteditable="true" placeholder="Click out to save" id="' +
-        e.layer._leaflet_id +
-        '"></div>';
-
-      e.layer.bindTooltip(tooltip, {
-        permanent: true,
-        direction: "bottom",
-        interactive: true
-      });
-
-      // disable dragging when cursor is over marker (tooltip)
-      // clicking on tooltip fires the marker's click handler, hence e.layer.on
-      e.layer.on("mouseover", this.props.changeDragState);
-
-      // enable dragging again when cursor is out of marker (tooltip)
-      e.layer.on("mouseout", this.props.changeDragState);
-
-      e.layer.on("click", this.checkDeleteModeStatus);
-
-      // show placeholder text again upon emptying textbox
-      e.layer.on("keyup", function() {
-        // when the text area is emptied, a <br> appears
-        // manually removing it so that the placeholder text can show
-        if (
-          tooltip.innerHTML ===
-            '<div placeholder="Click out to save" contenteditable="true" id ="' +
-              e.layer._leaflet_id +
-              "><br></div>" ||
-          tooltip.innerHTML ===
-            '<div placeholder="Click out to save" contenteditable="true" id ="' +
-              e.layer._leaflet_id +
-              "><div><br></div></div>"
-        ) {
-          tooltip.innerHTML =
-            '<div placeholder="Click out to save" contenteditable="true" id ="' +
-            e.layer._leaflet_id +
-            "></div>";
-        }
-      });
-
-      // blur event listener can't be given straight to a layer for some reason
-      // getting element by ID and adding an event listener to the element
-
-      document.getElementById(
-        e.layer._leaflet_id
-      ).onblur = this.makeGeoJSON.bind(this, e.layerType, e.layer);
-
-      document
-        .getElementById(e.layer._leaflet_id)
-        .addEventListener("focus", this._onEditStart);
-      document.getElementById(e.layer._leaflet_id).focus();
-      */
-
       this.createTextBox(e.layer, "");
-
       document.getElementById(e.layer._leaflet_id).focus();
-      this.setState({
-        createdTextBox: e.layer
-      });
       return;
     } // end if (e.layerType === "textbox")
 
-    if (e.layerType === "marker") {
-      this.setState({
-        createdMarker: e.layer
-      });
-      return;
-    }
-
-    this.addToAllLayers(e.layer);
     this.makeGeoJSON(e.layerType, e.layer);
     e.layer.remove();
   }; // end _onCreated
@@ -234,12 +159,6 @@ class DrawTools extends Component {
     document.getElementById(
       layer._leaflet_id
     ).onblur = this.checkTextOnBlur.bind(this, layer);
-
-    /*
-    document.getElementById(
-      layer._leaflet_id
-    ).onblur = this.checkTextOnBlur.bind(this, layer);
-    */
 
     // while textbox is on focus, it is being edited. however Leaflet's own edit mode is not on
     // need to determine manually that edit mode is on to avoid updating component, and hence losing focus from textbox every now and then
@@ -381,17 +300,6 @@ class DrawTools extends Component {
     return true;
   }
 
-  addToAllLayers(layer) {
-    /*
-    let allLayersUpdate = this.state.allLayers;
-    allLayersUpdate.push(layer);
-    this.setState({
-      allLayers: allLayersUpdate
-    });
-    console.log(this.state.allLayers);
-    */
-  }
-
   addToMap(feature) {
     let id = feature.mapDrawingId;
     let coords = feature.data.geometry.coordinates;
@@ -412,7 +320,6 @@ class DrawTools extends Component {
             id={id}
             radius={radius}
             color={color}
-            onAdd={e => this.addToAllLayers(e.target)}
           />
         );
       } else if (text || text === "") {
@@ -423,7 +330,6 @@ class DrawTools extends Component {
             id={id}
             color={color}
             icon={noIcon}
-            // onAdd={e => this.addToAllLayers(e.target)}
             onAdd={e => this.createTextBox(e.target, text)}
           />
         );
@@ -435,7 +341,6 @@ class DrawTools extends Component {
             position={position}
             id={id}
             color={color}
-            onAdd={e => this.addToAllLayers(e.target)}
           />
         );
       }
@@ -445,13 +350,7 @@ class DrawTools extends Component {
         return [coord[1], coord[0]];
       });
       return (
-        <Rectangle
-          key={Math.random()}
-          bounds={bounds}
-          id={id}
-          color={color}
-          onAdd={e => this.addToAllLayers(e.target)}
-        />
+        <Rectangle key={Math.random()} bounds={bounds} id={id} color={color} />
       );
     } else if (type === "Polygon") {
       // Polygon coordinates are wrapped under a one element array, for some reason
@@ -464,7 +363,6 @@ class DrawTools extends Component {
           positions={positions}
           id={id}
           color={color}
-          onAdd={e => this.addToAllLayers(e.target)}
         />
       );
     } else if (type === "LineString") {
@@ -478,221 +376,11 @@ class DrawTools extends Component {
           positions={positions}
           id={id}
           color={color}
-          onAdd={e => this.addToAllLayers(e.target)}
         />
       );
     }
     return null;
   }
-  /*
-
-  checkDifferences() {
-    let pos = 0;
-    let prevLayers = this.state.allLayers;
-    let nextLayers = this.props.geoJSONLayer.features;
-    let stopPoint = prevLayers.length - 1;
-    let layersToReturn = [];
-    let something = null;
-
-    for (let i = 0; i < nextLayers.length; i++) {
-      console.log(pos + " asd");
-      // check if there's more elements in the new layers than in previous layers
-      if (pos > stopPoint) {
-        layersToReturn.push(this.addToMap(nextLayers[pos]));
-        pos++;
-      } else if (
-        prevLayers[pos].options.id ===
-        nextLayers[nextLayers.length - 1].mapDrawingId
-      ) {
-      }
-
-      // check if IDs mismatch, which means the element has been removed
-      else if (prevLayers[pos].options.id !== nextLayers[pos].mapDrawingId) {
-        console.log(prevLayers[pos].options.id);
-        console.log(nextLayers[pos].mapDrawingId);
-        console.log(pos);
-        prevLayers.splice(pos, 1);
-      }
-
-      // check if element is a circle
-      else if (
-        prevLayers[pos]._mRadius &&
-        nextLayers[pos].data.properties.radius
-      ) {
-        console.log("check if element is a circle start");
-        something = this.checkCircleDifferences(
-          prevLayers[pos],
-          nextLayers[pos]
-        );
-        if (something !== null) {
-          console.log("circle has changed");
-          layersToReturn.push(something);
-        }
-        something = null;
-        pos++;
-      }
-
-      // check if element is a rectangle
-      else if (
-        prevLayers[pos].options.id === nextLayers[pos].mapDrawingId &&
-        nextLayers[pos].data.properties.rectangle
-      ) {
-        something = this.checkRectangleDifferences(
-          prevLayers[pos],
-          nextLayers[pos]
-        );
-        if (something !== null) {
-          layersToReturn.push(something);
-        }
-        pos++;
-      }
-
-      // check if element is a textbox
-      else if (
-        prevLayers[pos]._tooltip &&
-        nextLayers[pos].data.properties.text
-      ) {
-        something = this.checkTextBoxDifferences(
-          prevLayers[pos],
-          nextLayers[pos]
-        );
-        if (something !== null) {
-          layersToReturn.push(something);
-        }
-        pos++;
-      }
-
-      // check if element is a marker
-      else if (
-        prevLayers[pos]._latlng &&
-        nextLayers[pos].data.geometry.coordinates.length === 2
-      ) {
-        something = this.checkMarkerDifferences(
-          prevLayers[pos],
-          nextLayers[pos]
-        );
-        if (something !== null) {
-          layersToReturn.push(something);
-        }
-        pos++;
-      }
-
-      // check if element is either a Polygon or a Polyline
-      else if (prevLayers[pos]._latlngs) {
-        something = this.checkPolyDifferences(prevLayers[pos], nextLayers[pos]);
-        if (something !== null) {
-          layersToReturn.push(something);
-        }
-        pos++;
-      } else {
-        console.log("something weird happened");
-      }
-    } // end for
-
-    return layersToReturn;
-  }
-
-  checkCircleDifferences(prevLayer, nextLayer) {
-    let id = nextLayer.mapDrawingId;
-    let positions = [
-      nextLayer.data.geometry.coordinates[1],
-      nextLayer.data.geometry.coordinates[0]
-    ];
-    let color = nextLayer.data.properties.color;
-    let radius = nextLayer.data.properties.radius;
-    let status = false;
-    if (
-      prevLayer._mRadius !== nextLayer.data.properties.radius ||
-      prevLayer._latlng.lat !== nextLayer.data.geometry.coordinates[1] ||
-      prevLayer._latlng.lng !== nextLayer.data.geometry.coordinates[0]
-    ) {
-      status = true;
-    }
-
-    if (status === true) {
-      return (
-        <Circle
-          key={Math.random()} // keys are required to be able to edit
-          center={positions}
-          id={id}
-          radius={radius}
-          color={color}
-          onAdd={e => this.addToAllLayers(e.target)}
-        />
-      );
-    } else {
-      return null;
-    }
-  }
-
-  checkRectangleDifferences(prevLayer, nextLayer) {
-    let status = false;
-    console.log("rectangle check");
-  }
-
-  checkTextBoxDifferences(prevLayer, nextLayer) {
-    let status = false;
-    console.log("textbox check");
-  }
-
-  checkMarkerDifferences(prevLayer, nextLayer) {
-    let status = false;
-    console.log("textbox check");
-  }
-
-  checkPolyDifferences(prevLayer, nextLayer) {
-    let id = nextLayer.mapDrawingId;
-    let color = nextLayer.data.properties.color;
-    let positions;
-    let status = null;
-    if (nextLayer.data.geometry.type === "Polygon") {
-      status = "Polygon";
-      positions = nextLayer.data.geometry.coordinates[0].map(coord => {
-        return [coord[1], coord[0]];
-      });
-    } else {
-      status = "Polyline";
-      positions = nextLayer.data.geometry.coordinates.map(coord => {
-        return [coord[1], coord[0]];
-      });
-    }
-
-    let coordsLength = prevLayer._latlngs[0].length;
-    for (let i = 0; i < coordsLength - 1; i++) {
-      if (
-        prevLayer._latlngs[0][i].lat !==
-          nextLayer.data.geometry.coordinates[0][i][1] ||
-        prevLayer._latlngs[0][i].lng !==
-          nextLayer.data.geometry.coordinates[0][i][0]
-      ) {
-        status = true;
-        break;
-      }
-    }
-
-    if (status === "Polygon") {
-      return (
-        <Polygon
-          key={Math.random()}
-          positions={positions}
-          id={id}
-          color={color}
-          onAdd={e => this.addToAllLayers(e.target)}
-        />
-      );
-    } else if (status === "Polyline") {
-      return (
-        <Polyline
-          key={Math.random()}
-          positions={positions}
-          id={id}
-          color={color}
-          onAdd={e => this.addToAllLayers(e.target)}
-        />
-      );
-    }
-  }
-  */
 
   render() {
     return (
@@ -750,41 +438,9 @@ class DrawTools extends Component {
         )}
 
         {/* iterate through every element fetched from back-end */}
-        {this.props.geoJSONLayer.features.map((feature, index) => {
-          if (index === this.props.geoJSONLayer.features.length - 1) {
-            if (
-              this.state.createdTextBox !== null &&
-              feature.data.properties.text
-            ) {
-              console.log(feature);
-              let textbox = this.state.createdTextBox;
-              textbox.options.id = feature.mapDrawingId;
-              this.setState({
-                createdTextBox: null
-              });
-              return;
-            } else if (this.state.createdMarker !== null) {
-              console.log(feature);
-              let marker = this.state.createdMarker;
-              marker.options.id = feature.mapDrawingId;
-              this.setState({
-                createdMarker: null
-              });
-              return;
-            }
-          }
+        {this.props.geoJSONLayer.features.map(feature => {
           return this.addToMap(feature);
         })}
-
-        {/* 
-        {this.state.allLayers.length === 0
-          ? this.props.geoJSONLayer.features.map(feature => {
-              return this.addToMap(feature);
-            })
-          : this.checkDifferences().map(feature => {
-              return feature;
-            })}
-            */}
       </FeatureGroup>
     );
   }
